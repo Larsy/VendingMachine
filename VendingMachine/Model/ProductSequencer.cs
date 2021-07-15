1285e9e41f433ff4a32d12f2fa9e5029d00a2346
Larsy <lars@larsy.net>
2021-07-15 09:20:49 +0200

    Lagt till ProductSequencer

---
+++ VendingMachine/Model/ProductSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VendingMachine.Model
+{
+    class ProductSequencer
+    {
+        private static ushort productId = 0;
+        public static ushort ProductId
+        {
+            get { return productId; }
+        }
+        public static ushort NextId()
+        {
+            return productId++;
+        }
+        public static void Reset()
+        {
+            productId = 0;
+        }
+    }
+}
